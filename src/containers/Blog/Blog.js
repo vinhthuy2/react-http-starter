8399ae6b0d0af21ad21d68cc5114c6b1249883b5
Tran Vinh Thuy <thuy.tranvinh@vn.bosch.com>
2019-03-20 18:31:44 +0700

    http get example + data transformation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: []
+  };
+  componentDidMount = () => {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+      const posts = res.data.slice(0, 4);
+      const updatePosts = posts.map(post => {
+        return { ...post, author: 'Max' };
+      });
+      this.setState({ posts: updatePosts });
+    });
+  };
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} author={post.author} title={post.title} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
