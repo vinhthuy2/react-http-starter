78acc3db46bafca685aac704c46e69138440fb2c
Tran Vinh Thuy <thuy.tranvinh@vn.bosch.com>
2019-03-29 15:20:08 +0700

    lazy loading component added

--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -2,50 +2,54 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
-	render() {
-		return (
-			<div className="Blog">
-				<header>
-					<nav>
-						<ul>
-							<li>
-								<NavLink
-									to="/posts"
-									exact
-									activeClassName="my-active"
-									activeStyle={{ color: '#94391f' }}
-								>
-									Home
-								</NavLink>
-							</li>
-							<li>
-								<NavLink
-									activeClassName="active"
-									to={{
-										pathname: '/new-post',
-										hash: '#submit',
-										search: '?quick-submit=true'
-									}}
-								>
-									New post
-								</NavLink>
-							</li>
-						</ul>
-					</nav>
-				</header>
-				{/* <Route path="/" exact render={() => <Posts />} />
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/posts"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{ color: '#94391f' }}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  activeClassName="active"
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true'
+                  }}
+                >
+                  New post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <Posts />} />
                 <Route path="/" render={() => <h1>Home2</h1>} /> */}
-				<Switch>
-					<Route path="/new-post" exact component={NewPost} />
-					<Route path="/posts" component={Posts} />
-					<Redirect from="/" to="/posts" />
-				</Switch>
-			</div>
-		);
-	}
+        <Switch>
+          <Route path="/new-post" exact component={AsyncNewPost} />
+          <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Blog;
